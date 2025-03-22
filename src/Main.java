6e6930455480a671697b70b0b12c3f57e7219890
goli <shirafkanzeynab63@gmail.com>
2025-03-22 14:17:10 +0330

    add HashMap for rules

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import java.util.*;
+
 public class Main {
     public static final int MAX_VALUE = 1;
     public static final int MIN_VALUE = 100;
 
+    public final Map<Integer, String> rules = new HashMap<>();
+    
     public static void main(String[] args) {
 
     }
